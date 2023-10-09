243d311e3f7cddc5eab2fcd59382b4a955d6ced2
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2023-10-09 18:25:44 +0200

    Options block

--- src/main/kotlin/eu/automateeverything/sensorsandcontrollersplugin/CoreBlocksCollector.kt
+++ src/main/kotlin/eu/automateeverything/sensorsandcontrollersplugin/CoreBlocksCollector.kt
@@ -22,33 +22,33 @@ import eu.automateeverything.domain.hardware.*
 import org.pf4j.Extension
 
 @Extension
-class CoreBlocksCollector() : BlockFactoriesCollector {
+class CoreBlocksCollector : BlockFactoriesCollector {
 
     override fun collect(thisDevice: Configurable?): List<BlockFactory<*>> {
-        return listOf (
-            //temperature
+        return listOf(
+            // temperature
             ComparisonBlockFactory(Temperature::class.java, CommonBlockCategories.Temperature),
             EquationBlockFactory(Temperature::class.java, CommonBlockCategories.Temperature),
             TemperatureValueInCBlockFactory(),
             TemperatureValueInKBlockFactory(),
             TemperatureValueInFBlockFactory(),
 
-            //humidity
+            // humidity
             ComparisonBlockFactory(Humidity::class.java, CommonBlockCategories.Humidity),
             EquationBlockFactory(Humidity::class.java, CommonBlockCategories.Humidity),
             HumidityValueBlockFactory(),
 
-            //luminosity
+            // luminosity
             ComparisonBlockFactory(Luminosity::class.java, CommonBlockCategories.Luminosity),
             EquationBlockFactory(Luminosity::class.java, CommonBlockCategories.Luminosity),
             LuminosityValueBlockFactory(),
 
-            //wattage
+            // wattage
             ComparisonBlockFactory(Wattage::class.java, CommonBlockCategories.Wattage),
             EquationBlockFactory(Wattage::class.java, CommonBlockCategories.Wattage),
             WattageValueBlockFactory(),
 
-            //power level
+            // power level
             ComparisonBlockFactory(PowerLevel::class.java, CommonBlockCategories.PowerLevel),
             EquationBlockFactory(PowerLevel::class.java, CommonBlockCategories.PowerLevel),
             PowerLevelValueBlockFactory()
