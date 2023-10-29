ea4e2a695be3e8a3e27d96d19e081d9bd7e3d1d9
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2023-10-29 10:27:10 +0100

    Composition tab!!!! Nice!

--- src/main/kotlin/eu/automateeverything/sensorsandcontrollersplugin/CoreBlocksCollector.kt
+++ src/main/kotlin/eu/automateeverything/sensorsandcontrollersplugin/CoreBlocksCollector.kt
@@ -24,34 +24,41 @@ import org.pf4j.Extension
 @Extension
 class CoreBlocksCollector : BlockFactoriesCollector {
 
-    override fun collect(thisDevice: Configurable): List<BlockFactory<*>> {
-        return listOf(
-            // temperature
-            ComparisonBlockFactory(Temperature::class.java, CommonBlockCategories.Temperature),
-            EquationBlockFactory(Temperature::class.java, CommonBlockCategories.Temperature),
-            TemperatureValueInCBlockFactory(),
-            TemperatureValueInKBlockFactory(),
-            TemperatureValueInFBlockFactory(),
+    override fun collect(
+        thisDevice: Configurable,
+        context: CollectionContext
+    ): List<BlockFactory<*>> {
+        if (context == CollectionContext.Automation) {
+            return listOf(
+                // temperature
+                ComparisonBlockFactory(Temperature::class.java, CommonBlockCategories.Temperature),
+                EquationBlockFactory(Temperature::class.java, CommonBlockCategories.Temperature),
+                TemperatureValueInCBlockFactory(),
+                TemperatureValueInKBlockFactory(),
+                TemperatureValueInFBlockFactory(),
 
-            // humidity
-            ComparisonBlockFactory(Humidity::class.java, CommonBlockCategories.Humidity),
-            EquationBlockFactory(Humidity::class.java, CommonBlockCategories.Humidity),
-            HumidityValueBlockFactory(),
+                // humidity
+                ComparisonBlockFactory(Humidity::class.java, CommonBlockCategories.Humidity),
+                EquationBlockFactory(Humidity::class.java, CommonBlockCategories.Humidity),
+                HumidityValueBlockFactory(),
 
-            // luminosity
-            ComparisonBlockFactory(Luminosity::class.java, CommonBlockCategories.Luminosity),
-            EquationBlockFactory(Luminosity::class.java, CommonBlockCategories.Luminosity),
-            LuminosityValueBlockFactory(),
+                // luminosity
+                ComparisonBlockFactory(Luminosity::class.java, CommonBlockCategories.Luminosity),
+                EquationBlockFactory(Luminosity::class.java, CommonBlockCategories.Luminosity),
+                LuminosityValueBlockFactory(),
 
-            // wattage
-            ComparisonBlockFactory(Wattage::class.java, CommonBlockCategories.Wattage),
-            EquationBlockFactory(Wattage::class.java, CommonBlockCategories.Wattage),
-            WattageValueBlockFactory(),
+                // wattage
+                ComparisonBlockFactory(Wattage::class.java, CommonBlockCategories.Wattage),
+                EquationBlockFactory(Wattage::class.java, CommonBlockCategories.Wattage),
+                WattageValueBlockFactory(),
 
-            // power level
-            ComparisonBlockFactory(PowerLevel::class.java, CommonBlockCategories.PowerLevel),
-            EquationBlockFactory(PowerLevel::class.java, CommonBlockCategories.PowerLevel),
-            PowerLevelValueBlockFactory()
-        )
+                // power level
+                ComparisonBlockFactory(PowerLevel::class.java, CommonBlockCategories.PowerLevel),
+                EquationBlockFactory(PowerLevel::class.java, CommonBlockCategories.PowerLevel),
+                PowerLevelValueBlockFactory()
+            )
+        }
+
+        return listOf()
     }
 }
